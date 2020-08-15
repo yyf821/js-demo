aa2f7f6716f6e186f3d7e13bf693dd4b50ac1e82
yyf821 <yanyif21@gmail.com>
2020-08-15 19:44:36 +0900

    fix🐛 修复因乘除法·未加括号造成的结果遗漏 将所有步骤一律加上括号，最后统一去除多余括号

--- 24game/24.js
+++ 24game/24.js
@@ -40,14 +40,14 @@ const renderCards = function (cards) {
 const bindEventReset = function (cards) {
     let option = e('#card-reset')
     option.addEventListener('click', function () {
-        e('.answer').innerHTML=''
+        e('.answer').innerHTML = ''
         let arr = shuffle(cards)
         renderCards(arr)
     })
 }
 
 const getAllPossibleResult = function (a, b) {
-    return [`(${a}+${b})`, `(${a}-${b})`, `(${b}-${a})`, `${a}*${b}`, `${a}/${b}`, `${b}/${a}`];
+    return [`(${a}+${b})`, `(${a}-${b})`, `(${b}-${a})`, `(${a}*${b})`, `(${a}/${b})`, `(${b}/${a})`];
 }
 
 const judgePoint24 = function (nums) {
@@ -77,7 +77,10 @@ const judgePoint24 = function (nums) {
         // return false;
     }
     dfs(nums);
+    //结果去重
+    str = str.map(s => delExcessBrackets(s));
     str = Array.from(new Set(str))
+    str = filter(str, nums)
     return str
 };
 
@@ -96,7 +99,6 @@ const cardToNum = (arr) => {
 const showAnswer = () => {
     let divs = es('.card-img')
     let arr = []
-    let str = ''
     for (i = 0; i < divs.length; i++) {
         let num = divs[i].dataset.num
         arr.push(num)
@@ -112,7 +114,109 @@ const bindEventAnswer = function () {
         showAnswer()
     })
 }
+//全加，全乘算一种结果
+const filter = function (list, nums) {
+    let opList = ['+', '*']
+    let op = ''
+    for (let i = 0; i < list.length; i++) {
+        const exr = list[i];
+        for (let j = 0; j < opList.length; j++) {
+            const str = opList[j];
+            if (exr.split(str).length === 4) {
+                op = str
+                break
+            }
+        }
 
+    }
+    if (op) {
+        list = list.filter(word => word.split(op).length !== 4);
+        list.unshift(nums.join(op))
+    }
+    return list
+}
+
+
+
+//检测括号是否可以删除
+const check = function (s, left, right) {
+    let i;            //下标
+    let leftCount;    //左括号统计
+
+    //处理 ' -(a +|- b) '
+    if (s[left - 1] === '-') {
+        i = left;
+        leftCount = 1;
+        while (++i < right) {
+            if (s[i] === '(') {
+                leftCount++;
+            }
+            else if ((s[i] === '+' || s[i] === '-') && leftCount === 1) {
+                return false;
+            }
+        }
+    }
+
+    //处理 ' /(a +|-|*|/ b) '
+    if (s[left - 1] == '/') {
+        return false;
+    }
+
+    //处理 ' +(a +|-|*|/ b) +|- '
+    if (s[left - 1] != '*' && s[left - 1] != '/' &&
+        s[right + 1] != '*' && s[right + 1] != '/') {
+        return true;
+    }
+
+    //处理 ' *(a *|/ b) +|-|*|/ '
+    i = left;
+    leftCount = 1;
+    while (++i < right) {
+        if (s[i] == '(') {
+            leftCount++;
+        }
+        else if ((s[i] == '*' || s[i] == '/') && leftCount == 1) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//删除多余的括号
+const delExcessBrackets = function (s) {
+    let right = 0;
+    let left = 0;
+    let stack = []
+    let del = []
+    for (let i = 0; i < s.length; i++) {
+        // 遍历，碰到(直接下标入栈，碰到)出栈
+        if (s[i] === '(') {
+            stack.push(i);
+        } else if (s[i] === ')') {
+            left = stack.pop()
+            right = i
+            //若检测结果为可以删除，那么标记括号位置删除
+            if (check(s, left, right)) {
+                del.push(left)
+                del.push(right)
+            }
+        }
+    }
+
+    if (del) {
+        del = del.sort(function (a, b) {
+            return a - b;
+        });
+        s = s.split('')
+        for (let i = 0; i < del.length; i++) {
+            const n = del[i];
+            s.splice(n - i, 1)
+        }
+        s = s.join('')
+    }
+    return s
+
+}
 
 const __main = () => {
     let cards = initCards()
