d76ec8dd16debdb150cfeae7759063cae5eb8eb9
yyf821 <zlsbd01@126.com>
2020-09-08 23:50:28 +0900

    feat✨ 通过checkbox管理tree view

--- treeView/treeview.js
+++ treeView/treeview.js
@@ -36,14 +36,21 @@ const treeToList = function (arr) {
     let menus = '';
     function inner(arr) {
         arr.forEach((element) => {
+            let { id, pid, name } = element
+            let t = `
+            <input type="checkbox" data-id="${id}" data-pid="${pid}" class="cbox" id="cbox${id}" value="${name}">
+            <label for="cbox${id}">${name}</label>
+            `
             if (element.children.length > 0) {
-                menus += `<li><span class="caret">${element.name}</span>`;
-                menus += '<ul class="nested">';
+                menus += `<li><span class="caret">
+                ${t}
+                </span>`;
+                menus += '<ul class="nested">'
                 inner(element.children)
                 menus += '</ul>';
                 menus += '</li>';
             } else {
-                menus += `<li>${element.name}</li>`;
+                menus += `<li>${t}</li>`
             }
 
         })
@@ -65,17 +72,70 @@ const bindEventClick = function () {
         let self = event.target
         if (self.classList.contains("caret")) {
             //找到公共父元素
-            let el = self.closest("li").querySelector(".nested");
-            el.classList.toggle("active");
-            self.classList.toggle("caret-down");
+            let el = self.closest("li").querySelector(".nested")
+            el.classList.toggle("active")
+            self.classList.toggle("caret-down")
         }
     })
 }
 
+const setChecked = function () {
+    let checkBoxes = es(".cbox");
+    checkBoxes.forEach((box) => {
+        box.addEventListener('click', (event) => {
+            childCheck(event.target)
+            parentCheck(event.target)
+        });
+    });
+}
+
+//点击父元素选择所有子项目
+const childCheck = function (box) {
+    let ul = box.closest("li").querySelector(".nested");
+    if (ul) {
+        let children = ul.querySelectorAll(".cbox");
+        children.forEach((child) => {
+            child.checked = box.checked
+            childCheck(child)
+        });
+    }
+}
+
+//判断所有兄弟元素是否都被选中
+const isAllBrosCheck = function (box) {
+    let bros = box.closest("ul").querySelectorAll(".cbox");
+    for (let i = 0; i < bros.length; i++) {
+        const bro = bros[i];
+        if (!bro.checked) {
+            return false
+        }
+    }
+    return true
+}
+
+//取消选择一个元素，父元素也会取消选择
+//如果所有子项目都是选中状态，父元素也会被选中
+const parentCheck = function (box) {
+    let pid = Number(box.dataset.pid)
+    if (pid !== 0) {
+        let id = `#cbox${pid}`
+        let parent = e(id)
+        if (!box.checked) {
+            parent.checked = false
+            parentCheck(parent)
+        } else {
+            if (isAllBrosCheck(box)) {
+                parent.checked = true
+                parentCheck(parent)
+            }
+        }
+    }
+}
 
 const __main = () => {
     renderTree()
     bindEventClick()
+    setChecked()
 }
 
 __main()
